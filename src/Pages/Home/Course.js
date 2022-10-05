88f3accd4c88c05f36e07b3cd2d0537679779366
Rafsan12 <rafsan.web.2022@gmail.com>
2022-10-05 18:52:50 +0600

    dom

---
+++ src/Pages/Home/Course.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import course_poster from "../../Course_image/COURSE Poster.gif"
+
+const Course = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src={course_poster} className="max-w-sm rounded-lg shadow-2xl" alt='' />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+            
+        </div>
+    );
+};
+
+export default Course;
