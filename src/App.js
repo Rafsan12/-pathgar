7b3054dde6e5cbe53334da03bf9cac5a413a42d1
Rafsan12 <rafsan.web.2022@gmail.com>
2022-10-05 19:10:06 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from "../src/Pages/Home/Home"
 
 function App() {
   return (
-    <div className="App">
-  
+    <div >
+   <Routes>
+    <Route path='/' element={<Home></Home>}></Route>
+   </Routes>
 
     </div>
   );
