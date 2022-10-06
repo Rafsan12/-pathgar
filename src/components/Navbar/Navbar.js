081468a24ece246761281d16dfaa7ae33bc8cea3
sadmanalam142 <sadmanalam142@gmail.com>
2022-10-06 22:54:06 +0600

    style added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,16 +5,22 @@ const Navbar = () => {
         <div>
             <div className="navbar bg-primary">
                 <div className="navbar-start">
-                </div>
-                <div className="navbar-center">
                     <a className="btn btn-ghost normal-case text-xl">Pathagar</a>
                 </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal p-0">
+                    <li><a>Home</a></li>
+                            <li><a>Books</a></li>
+                            <li><a>Courses</a></li>
+                            <li><a>Login</a></li>
+                    </ul>
+                </div>
                 <div className="navbar-end">
-                    <div className="dropdown mr-12">
-                        <label tabIndex={0} className="btn btn-ghost btn-circle">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                <div className="dropdown mr-10">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-primary rounded-box w-52">
                             <li><a>Home</a></li>
                             <li><a>Books</a></li>
                             <li><a>Courses</a></li>
@@ -23,6 +29,7 @@ const Navbar = () => {
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
