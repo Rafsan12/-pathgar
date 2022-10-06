da54373bdc2cb3ba5b966d60599c7a8e263406b5
Alamgir4597 <rrony45@gmail.com>
2022-10-07 00:14:07 +0600

    footer from Alamgir

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <h2>Hello world</h2>
       <button class="btn w-64 rounded-full">Button</button>
-
+<Footer />
     </div>
   );
 }
