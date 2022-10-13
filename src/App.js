78ba5ff400336ceab01cbd6d2e89d9162314c037
Rafsan12 <rafsan.web.2022@gmail.com>
2022-10-13 18:49:03 +0600

    button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+// import CourseDetails from './Pages/Home/Course/CourseDetails';
 import Courses from './Pages/Home/Course/Courses';
 
 import Home from './Pages/Home/Home';
@@ -10,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home></Home>}></Route>
       <Route path="/courses" element={<Courses></Courses>}></Route>
+      {/* <Route path="/course/:coursesId" element={<CourseDetails></CourseDetails>}></Route> */}
+      {/* <Route path="/courseDetail" element={<CourseDetails></CourseDetails>}></Route> */}
     </Routes>
     </div>
   );
--- src/Pages/Home/Course/ShowCourse.js
+++ src/Pages/Home/Course/ShowCourse.js
@@ -1,17 +1,21 @@
 import React from 'react';
-
-
-const ShowCourse = ({course}) => {
+import {  useNavigate } from 'react-router-dom';
+const ShowCourse = (props) => {
+    const {_id,image,name,price} = props.course;
+    const navigate = useNavigate
+const navigateCourseDetails = id =>{
+    navigate(`/course/${id}`)
+}
     return (
         <div>
         <figure className='pt-5'>
-            <img src={course.image} className='h-44 mx-auto p-3 rounded' alt=''/>
+            <img src={image} className='h-44 mx-auto p-3 rounded' alt=''/>
             </figure>
         <div className="card-body">
-            <h2 className="card-title">{course.name}</h2>
-            <p>Price: ${course.price}</p>
+            <h2 className="card-title">{name}</h2>
+            <p>Price: ${price}</p>
             <div className="card-actions justify-end">
-                <button className="text-white text-xl btn btn-primary  hover:bg-[#293661] duration-500 rounded-lg px-8 py-3.5 flex items-center'">Add to Cart</button>
+                <button onClick={() => navigateCourseDetails(_id)} className="text-white text-xl btn btn-primary  hover:bg-[#293661] duration-500 rounded-lg px-8 py-3.5 flex items-center'">Add To Cart</button>
             </div>
         </div>
     </div>
