967d67e61f04871202f8794cbf288ff23a39fcba
Rafsan12 <rafsan.web.2022@gmail.com>
2022-10-14 09:10:49 +0600

    invisible

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,8 +14,9 @@ const Navbar = () => {
   const menuItems = <>
     <li><Link to="/">Home</Link></li>
     <li><Link to="/about">About</Link></li>
-    <li><Link to="/login">Login</Link></li>
     <li> <Link to='/all-books'>Books</Link> </li>
+    <li><Link to="/login">Login</Link></li>
+   
 
   </>
 
@@ -36,7 +37,7 @@ const Navbar = () => {
             <label tabIndex={0} className="btn btn-ghost lg:hidden ">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow   rounded-box w-52   text-white">
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow   rounded-box w-52   text-black">
               {menuItems}
             </ul>
           </div>
@@ -55,8 +56,8 @@ const Navbar = () => {
         </div>
         <div className="navbar-end p-4">
           <div className="indicator cursor-pointer" onClick={handleCartCourse}>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-            <span className="badge badge-sm indicator-item badge-success">{getCourseData.length}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 invisible" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+            <span className="badge badge-sm indicator-item badge-success invisible">{getCourseData.length}</span>
           </div>
         </div>
       </div>
