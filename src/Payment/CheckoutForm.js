56580f865623980e9e0c3fdffb82ddd1d9c18ba3
solaymanWeb <solaymanweb27@gmail.com>
2022-10-21 15:23:05 +0600

    test code

--- src/Payment/CheckoutForm.js
+++ src/Payment/CheckoutForm.js
@@ -4,57 +4,57 @@ import React, { useState } from 'react';
 const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
-    const [cardError, setCardError]=useState(" ");
+    const [cardError, setCardError] = useState(" ");
 
-    const handleSubmit= async(event)=>{
+    const handleSubmit = async (event) => {
         event.preventDefault()
-        if(!stripe || !elements){
+        if (!stripe || !elements) {
             return;
         }
 
         const card = elements.getElement(CardElement);
-        if(card === null){
+        if (card === null) {
             return;
         }
 
-        const {error, paymentMethod} = await stripe.createPaymentMethod({
-            type:'card',
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
             card,
         })
-        if(error){
+        if (error) {
             setCardError(error.message)
-        }else{
+        } else {
             setCardError()
         }
     }
 
     return (
         <>
-             <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: '16px',
-              color: '#424770',
-              padding: '5px',
-              '::placeholder': {
-                color: '#aab7c4',
-              },
-            },
-            invalid: {
-              color: '#9e2146',
-            },
-          },
-        }}
-      />
-      <button className='btn btn-success btn-sm px-5 mt-5 text-white' type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
-    {
-        cardError && <p className='text-red-500'>{cardError}</p>
-    }
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                padding: '5px',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button className='btn btn-success btn-sm px-5 mt-5 text-white' type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
         </>
     );
 };
